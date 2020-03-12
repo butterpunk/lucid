b024705968f09a18c4e4a7eb522492395fe80f90
Blake butterworth <blakebutterworth@Blakes-MBP.attlocal.net>
2020-03-12 18:00:19 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './client/App';
+import  App  from './client/App/index.js';
+import store from './store';
+import { Provider } from "react-redux";
+const render = () => {
+    ReactDOM.render(
+    (<Provider store={store}>
+        <App />
+    </Provider>), 
+        document.getElementById('root'));
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+store.subscribe(render);
+render();
